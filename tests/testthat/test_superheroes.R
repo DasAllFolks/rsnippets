2f50dc9ac7ec5f9193716b063ec61852b0407940
Steven Das <steven.c.das@gmail.com>
2015-06-27 18:11:55 -0500

    Remove redundant tests which would really just test rep

--- tests/testthat/test_superheroes.R
+++ tests/testthat/test_superheroes.R
@@ -1,13 +1,5 @@
 context('Superheroes')
 
-test_that('batman method errors for a non-integer input', {
-    expect_error(batman(0.2352))
-})
-
-test_that('batman method errors for a negative integral input', {
-    expect_error(batman(-1))
-})
-
 test_that('batman returns singleton list with "Batman!" for a zero input', {
     expect_equal(batman(0), c('Batman!'))
 })
