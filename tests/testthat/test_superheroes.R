0095c69734b797b4fb6a0b875a43d9dbf0be2544
Steven Das <steven.c.das@gmail.com>
2015-06-27 17:28:10 -0500

    Beginnings of superhero unit tests

---
+++ tests/testthat/test_superheroes.R
@@ -0,0 +1,17 @@
+context('Superheroes')
+
+test_that('batman method errors for a non-integer input', {
+    # TODO
+})
+
+test_that('batman method errors for a negative integral input', {
+    # TODO
+})
+
+test_that('batman method just returns "Batman!" for a zero input', {
+    # TODO
+})
+
+test_that('batman method returns appropriate number of Nans for input', {
+    # TODO
+})
