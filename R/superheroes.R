8a2f989a4edfec0d98b2a18e065cece04e4d08db
Steven Das <steven.c.das@gmail.com>
2015-06-27 18:18:12 -0500

    Remove example from docs section altogether

--- R/superheroes.R
+++ R/superheroes.R
@@ -2,8 +2,6 @@
 #'
 #' @param n A number.  Restrictions are the same as for the times param of rep.
 #' @return A Batman-themed list.
-#' @examples
-#' Run it and see for yourself. ;-)
 batman <- function(n) {
     list_ <- rep(NaN, n)
     list_[n] <- 'Batman!'
