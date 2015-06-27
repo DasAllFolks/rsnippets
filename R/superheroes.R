e786099938ebabbaaaeb8c2f41018ed33557b591
Steven Das <steven.c.das@gmail.com>
2015-06-27 15:03:59 -0500

    Batman begins

---
+++ R/superheroes.R
@@ -0,0 +1,9 @@
+#' Replicate an extremely cheesy theme song.
+#'
+#' @param n A positive integer.
+#' @return A Batman-themed list.
+#' @examples
+#' You'll just have to run it and see for yourself. ;-)
+batman <- function(n) {
+  # TODO
+}
