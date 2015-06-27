d0ac712c60d09493dc414edea1c3239f51cc9567
Steven Das <steven.c.das@gmail.com>
2015-06-27 18:17:10 -0500

    Remove quote throwing off roxygen

--- R/superheroes.R
+++ R/superheroes.R
@@ -3,7 +3,7 @@
 #' @param n A number.  Restrictions are the same as for the times param of rep.
 #' @return A Batman-themed list.
 #' @examples
-#' You'll just have to run it and see for yourself. ;-)
+#' Run it and see for yourself. ;-)
 batman <- function(n) {
     list_ <- rep(NaN, n)
     list_[n] <- 'Batman!'
