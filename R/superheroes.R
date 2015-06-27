fce6e6b8333593a52dc59054a889286efd7991f7
Steven Das <steven.c.das@gmail.com>
2015-06-27 18:11:11 -0500

    Clarify domain restrictions

--- R/superheroes.R
+++ R/superheroes.R
@@ -1,11 +1,9 @@
 #' Replicate an extremely cheesy theme song.
 #'
-#' @param n A positive integer.
+#' @param n A number.  Restrictions are the same as for the times param of rep.
 #' @return A Batman-themed list.
 #' @examples
 #' You'll just have to run it and see for yourself. ;-)
 batman <- function(n) {
-    if (n < 0) {
-        stop('n must be a positive integer')
-    }
+    # TODO
 }
